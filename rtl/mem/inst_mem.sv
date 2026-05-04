1114c2f2ae1c115e9315296a825cf581b8022698
sheepkaz0814 <sheepkaz0814@users.noreply.github.com>
2026-05-05 01:23:06 +0900

    add inst_mem.sv

---
+++ rtl/mem/inst_mem.sv
@@ -0,0 +1,16 @@
+module inst_mem (
+  input  logic [31:0] addr,
+  output logic [31:0] instr
+);
+
+  logic [31:0] mem [0:15];
+
+  initial begin
+    // プログラム書く（後でテスト）
+    mem[0] = 32'b000000000101_00000_000_00001_0010011; // ADDI r1, r0, 5
+    mem[1] = 32'b000000000010_00001_000_00010_0010011; // ADDI r2, r1, 2
+  end
+
+  assign instr = mem[addr[5:2]]; // wordアドレス
+
+endmodule
